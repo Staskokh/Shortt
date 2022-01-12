bbdae3579cdffe36f83acd1f906d54547bfe739c
Staskokh <lion.tv.02@bk.ru>
2022-01-12 18:56:46 +0700

    qq1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int A,B;
+        A=12;
+        B=23;
+        Scanner in;
+        in=new Scanner(System.in);
+                A=in.nextInt();
+                B=in.nextInt();
+        System.out.println(A+B);
+    }
+}
